4e7b35275f73d0c7eb1a36671093bcf2d5b3fedb
Roman Kulyniak <roman.kulyniak@gmail.com>
2024-02-27 21:51:31 +0200

    add a functional test that checks context can be loaded

---
+++ core/src/test/java/com/splitbill/core/SplitBillCoreWebAppTest.java
@@ -0,0 +1,12 @@
+package com.splitbill.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitBillCoreWebAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
