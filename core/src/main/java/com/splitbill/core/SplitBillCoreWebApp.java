1422ffef50a96aa03a4bdc39563fdf0a3e499f27
Roman Kulyniak <roman.kulyniak@gmail.com>
2024-02-27 21:49:20 +0200

    rename package from `com.splitthebill` to `com.splitbill`

--- core/src/main/java/com/splitbill/core/SplitBillCoreWebApp.java
+++ core/src/main/java/com/splitbill/core/SplitBillCoreWebApp.java
@@ -1,12 +1,12 @@
-package com.splitthebill.core;
+package com.splitbill.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreWebApplication {
+public class SplitBillCoreWebApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoreWebApplication.class, args);
+        SpringApplication.run(SplitBillCoreWebApp.class, args);
     }
 }
